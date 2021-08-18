d61117f0c25b07a8a46c3a658ae409c366f23bc1
bennettscience <brian.bennett2@gmail.com>
2021-08-18 14:52:09 -0400

    check for anonymous user before logging

--- app/logging.py
+++ app/logging.py
@@ -1,8 +1,9 @@
 import json
 from flask import request
+from flask_migrate import current
 from app import db
 from app.models import Log
-from flask_login import current_user
+from flask_login import current_user, AnonymousUserMixin
 
 def create_log():
     data = None
@@ -12,9 +13,14 @@ def create_log():
 
     if request.json is not None:
         data = request.json
+    
+    if current_user.is_anonymous:
+        user_id = 0
+    else:
+        user_id = current_user.id
 
     log = Log(
-        user_id=6,
+        user_id=user_id,
         source_uri=source_uri,
         endpoint=endpoint,
         method=method,
