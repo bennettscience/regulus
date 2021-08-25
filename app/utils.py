8edfac662597aa1a58ddb58ce93d4c9d1a85adea
bennettscience <brian.bennett2@gmail.com>
2021-08-24 20:56:55 -0400

    User input sanitization
    The frontend includes a rich text editor for descriptions. This module
    adds HTML sanitization before commiting descriptions to the database
    to prevent XSS attacks.
    
    Allowed tags are p, strong, em, br, ul, ol, li

---
+++ app/utils.py
@@ -0,0 +1,27 @@
+import bleach_extras
+from html import unescape
+
+
+def clean_escaped_html(value: str) -> str:
+    """ Remove non-whitelist HTML tags from user input.
+
+    On the frontend, Jodit escapes HTML characters on the fly, sending unicode strings. Those need
+    to be removed before processing with bleach to remove non-whitelisted tags.
+
+    This is a little goofy because the escaped HTML needs to be parsed back into unicode characters
+    to be removed correctly by bleach.
+
+    The bleach_extras package removes non-whitelisted tag children elements as well as the tags.
+
+    This solution is a combination of approaches from 
+      - https://stackoverflow.com/questions/701704/convert-html-entities-to-unicode-and-vice-versa
+      - https://github.com/jvanasco/bleach_extras
+
+    Args:
+        value (str): string containing HTML
+
+    Returns:
+        str: Sanitized HTML
+    """
+    clean = bleach_extras.clean_strip_content(unescape(value), tags=['p', 'strong', 'em', 'br', 'ul', 'ol', 'li'])
+    return clean
