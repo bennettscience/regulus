b1937c59f0d8f0ac2ff38f8afc5541b1a6c5ba7e
bennettscience <brian.bennett2@gmail.com>
2022-06-21 15:22:37 -0400

    fix logs to handle form data

--- app/logging.py
+++ app/logging.py
@@ -11,8 +11,15 @@ def create_log():
     source_uri = request.remote_addr
     method = request.method
 
-    if request.json is not None:
-        data = request.json
+    # requests are either JSON or form inputs. Update
+    # to handle any data type.
+    # See https://stackoverflow.com/questions/45590988/converting-flask-form-data-to-json-only-gets-first-value
+    # for handling form input serialization for logs
+    if method == 'POST' or method == 'PUT':
+        if request.form is not None and request.is_json is False:
+            data = json.dumps(request.form)
+        elif request.json is not None:
+            data = json.dumps(request.json)
     
     user_id = current_user.id
 
@@ -21,7 +28,7 @@ def create_log():
         source_uri=source_uri,
         endpoint=endpoint,
         method=method,
-        json_data=json.dumps(data)
+        json_data=data
     )
 
     db.session.add(log)
