254520d8c58f2a5be9545db7c5ec93bc3e02ad48
bennettscience <brian.bennett2@gmail.com>
2022-06-21 15:34:14 -0400

    remove redundant routes

--- app/__init__.py
+++ app/__init__.py
@@ -76,14 +76,7 @@ from resources.locations import (
     LocationListAPI,
     LocationUsersAPI,
 )
-from resources.users import (
-    UserAPI,
-    UserAttendingAPI,
-    UserListAPI,
-    UserLocationAPI,
-    UserPresentingAPI,
-    UserConfirmedAPI
-)
+
 from resources.usertypes import UserTypesAPI
 from app.schemas import UserSchema, CourseSchema, LogSchema
 
@@ -95,18 +88,10 @@ from app.blueprints.home_blueprint import home_bp
 from app.blueprints.locations_blueprint import locations_bp
 from app.blueprints.users_blueprint import users_bp
 
-course_types_view = CourseTypesAPI.as_view("course_types_api")
 course_type_view = CourseTypeAPI.as_view("course_type_api")
-course_links_view = CourseLinksAPI.as_view("course_links_api")
-course_link_view = CourseLinkAPI.as_view("course_link_api")
 course_linktypes_view = CourseLinkTypesAPI.as_view("course_linktypes_api")
 course_linktype_view = CourseLinkTypeAPI.as_view("course_linktype_api")
-course_presenters_view = CoursePresentersAPI.as_view("course_presenters_api")
-course_presenter_view = CoursePresenterAPI.as_view("course_presenter_api")
-# locations_view = LocationListAPI.as_view("locations_api")
-# location_view = LocationAPI.as_view("location_api")
-# location_user_view = LocationUsersAPI.as_view("location_user_api")
-# location_course_view = LocationCoursesAPI.as_view("location_courses_api")
+
 user_types_view = UserTypesAPI.as_view("user_types_api")
 
 # Register all the blueprints
@@ -121,24 +106,7 @@ app.register_blueprint(users_bp)
 def load_user(id):
     return User.query.get(id)
 
-
-
-# @app.route('/')
-# @app.route('/schedule')
-# @app.route('/presenter')
-# @app.route('/admin')
-# @app.route('/create')
-# @app.route('/documents')
-# @app.route('/people')
-# def base():
-#     return send_from_directory('client/public', 'index.html')
-
-
-# @app.route("/<path:path>")
-# def home(path):
-#     return send_from_directory('client/public', path)
-
-
+# Authorization routes run on the main app instead of through a blueprint
 @app.route("/authorize/<provider>")
 def oauth_authorize(provider):
     # redirect_uri = url_for('auth', _external=True)
@@ -178,30 +146,12 @@ def check_session():
         return jsonify({"login": True, "user": UserSchema().dump(user)})
     return jsonify({"login": False})
 
-@app.route("/presenters")
-def get_presenters():
-    presenters = User.query.join(User.role, aliased=True).filter_by(name='Presenter').all()
-    return jsonify(UserSchema(many=True).dump(presenters))
-
-@app.route("/popular")
-def get_popular_course():
-    # pass
-    course = Course.query(func.max(Course.registrations)).one()
-    return jsonify(CourseSchema().dump(course))
-
 
 # Logging
 @app.before_request
 def log_request():
     if not current_user.is_anonymous:
         create_log()
-
-# Attach Cahce-Control header to all responses
-# Cache results for 60 minutes. After that, refresh from the server.
-# @app.after_request
-# def add_header(response):
-#     response.headers['Cache-Control'] = 'max-age=3600, must-revalidate'
-#     return response
 
 # Request all logs for an event
 @app.route("/logs/<int:course_id>", methods=['GET'])
@@ -210,51 +160,6 @@ def get_logs(course_id):
     return jsonify(LogSchema(many=True).dump(query))
 
 
-# CRUD endpoints
-# app.add_url_rule("/courses", view_func=courses_view, methods=["GET", "POST"])
-# app.add_url_rule(
-#     "/courses/<int:course_id>", view_func=course_view, methods=["GET", "PUT", "DELETE"]
-# )
-# app.add_url_rule(
-#     "/courses/<int:course_id>/links",
-#     view_func=course_links_view,
-#     methods=["GET", "POST"],
-# )
-# app.add_url_rule(
-#     "/courses/<int:course_id>/links/<int:link_id>",
-#     view_func=course_link_view,
-#     methods=["GET", "PUT", "DELETE"],
-# )
-# app.add_url_rule(
-#     "/courses/<int:course_id>/presenters",
-#     view_func=course_presenters_view,
-#     methods=["GET", "POST"],
-# )
-# app.add_url_rule(
-#     "/courses/<int:course_id>/presenters/<int:user_id>",
-#     view_func=course_presenter_view,
-#     methods=["POST", "DELETE"],
-# )
-# app.add_url_rule(
-#     "/courses/<int:course_id>/registrations",
-#     view_func=course_attendees_view,
-#     methods=[
-#         "GET",
-#         "PUT",
-#         "POST",
-#     ],
-# )
-# app.add_url_rule(
-#     "/courses/<int:course_id>/registrations/<int:user_id>",
-#     view_func=course_attendee_view,
-#     methods=["PUT", "POST", "DELETE"],
-# )
-# app.add_url_rule(
-#     "/courses/types", view_func=course_types_view, methods=["GET", "POST"]
-# )
-# app.add_url_rule(
-#     "/courses/types/<int:coursetype_id>", view_func=course_type_view, methods=["GET", "PUT", "DELETE"]
-# )
 app.add_url_rule(
     "/courselinktypes", view_func=course_linktypes_view, methods=["GET", "POST"]
 )
