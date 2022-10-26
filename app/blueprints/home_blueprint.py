af2584a830e44394bcbce06f514fab84fe8ba428
bennettscience <brian.bennett2@gmail.com>
2022-10-26 14:41:33 -0400

    remove unused imports

--- app/blueprints/home_blueprint.py
+++ app/blueprints/home_blueprint.py
@@ -1,35 +1,36 @@
 from typing import List
-from flask import Blueprint, redirect, render_template, request, session, url_for
+from flask import Blueprint, render_template, request
 from flask_login import current_user
 
 from app.utils import get_user_navigation
 from app.wrappers import restricted
 
-home_bp = Blueprint('home_bp', __name__)
+home_bp = Blueprint("home_bp", __name__)
 
 
-@home_bp.get('/')
+@home_bp.get("/")
 def index():
     if current_user.is_anonymous:
-        return render_template('auth/login.html')
+        return render_template("auth/login.html")
     else:
         nav_items = get_user_navigation()
-    
-        if request.headers.get('HX-Request'):
-            return render_template('home/index-partial.html', menuitems=nav_items)
-        else:
-            return render_template('home/index.html', menuitems=nav_items)
 
-@home_bp.get('/create')
+        if request.headers.get("HX-Request"):
+            return render_template("home/index-partial.html", menuitems=nav_items)
+        else:
+            return render_template("home/index.html", menuitems=nav_items)
+
+
+@home_bp.get("/create")
 @restricted
 def create():
     from app.models import CourseType, Location
     from app.schemas import CourseTypeSchema, LocationSchema
 
-    if request.headers.get('HX-Request'):
-        template = 'admin/forms/create-form-partial.html'
+    if request.headers.get("HX-Request"):
+        template = "admin/forms/create-form-partial.html"
     else:
-        template = 'admin/forms/create-form-full.html'
+        template = "admin/forms/create-form-full.html"
 
     nav_items = get_user_navigation()
     course_types = CourseType.query.all()
@@ -38,7 +39,7 @@ def create():
     content = {
         "course_types": CourseTypeSchema(many=True).dump(course_types),
         "locations": LocationSchema(many=True).dump(locations),
-        "menuitems": nav_items
+        "menuitems": nav_items,
     }
 
     return render_template(template, **content)
