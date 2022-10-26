c011ef94ef69cbb9dd7c0fc4410008591312c6d9
bennettscience <brian.bennett2@gmail.com>
2022-10-26 15:25:16 -0400

    update user access controls

--- resources/users.py
+++ resources/users.py
@@ -25,49 +25,6 @@ from app.schemas import (
 from app.utils import get_user_navigation
 
 
-class UserListAPI(MethodView):
-    # @parser.use_kwargs({'user_type': fields.Int()}, location="querystring")
-    # @restricted
-    # def get(self: None, user_type=None) -> List[User]:
-    #     """ Get a list of all users.
-
-    #     Returns:
-    #         List[User]: List of users.
-    #     """
-    #     breakpoint()
-    #     if current_user.usertype_id != 1 and user_type != 1:
-    #         # presenters and admins can request non-admins
-    #         users = User.query.filter_by(usertype_id=user_type).all()
-    #     elif current_user.usertype_id == 1 and user_type:
-    #         users = User.query.filter_by(usertype_id=user_type).all()
-    #     elif current_user.usertype_id == 1 and user_type is None:
-    #         users = User.query.all()
-    #     else:
-    #         abort(422)
-
-    #     # TODO: Sort users by last name
-    #     sorted_users = sorted(users)
-    #     return jsonify(UserSchema(many=True).dump(sorted_users))
-
-    def post(self: None) -> User:
-        """ Create a new user
-
-        Returns:
-            User: JSON representation of the user
-        """
-        args = parser.parse(NewUserSchema(), location="json")
-
-        if current_user.usertype_id != 1:
-            abort(403)
-
-        try:
-            user = User().create(User, args)
-            result = User.query.get(user.id)
-            return jsonify(UserSchema().dump(result))
-        except Exception as e:
-            return jsonify(e)
-
-
 class UserAPI(MethodView):
     @admin_or_self
     def get(self: None, user_id: int) -> User:
@@ -79,7 +36,10 @@ class UserAPI(MethodView):
         Returns:
             User: JSON representation of the user.
         """
-        options = [{"value": location.id, "text": location.name} for location in Location.query.all()]
+        options = [
+            {"value": location.id, "text": location.name}
+            for location in Location.query.all()
+        ]
         user = User.query.get(user_id)
 
         content = {
@@ -87,14 +47,14 @@ class UserAPI(MethodView):
             "user": user,
         }
         return render_template(
-            'shared/partials/sidebar.html',
-            partial='users/partials/user-edit-account-form.html',
+            "shared/partials/sidebar.html",
+            partial="users/partials/user-edit-account-form.html",
             **content
         )
 
     @admin_or_self
     def put(self: None, user_id: int) -> User:
-        """ Update a user's details
+        """Update a user's details
 
         Args:
             user_id (int): valid user ID
@@ -106,20 +66,23 @@ class UserAPI(MethodView):
         required = {
             "name": fields.Str(),
             "location_id": fields.Int(),
-            "usertype_id": fields.Int()
+            "usertype_id": fields.Int(),
         }
         # Limit this to SuperAdmins or the user making the request.
         if current_user.usertype_id == 1 or current_user.id == user_id:
             args = parser.parse(required, location="form")
-           
+
             user = User.query.get(user_id)
             if user is None:
                 abort(404)
 
             try:
                 user.update(args)
-                response = make_response('ok')
-                response.headers.set('HX-Trigger', json.dumps({'showToast': 'Successfully updated the user.'}))
+                response = make_response("ok")
+                response.headers.set(
+                    "HX-Trigger",
+                    json.dumps({"showToast": "Successfully updated the user."}),
+                )
                 return response
                 # return jsonify(UserSchema().dump(user))
             except Exception as e:
@@ -129,7 +92,7 @@ class UserAPI(MethodView):
 
     @admin_only
     def delete(self: None, user_id: int) -> dict:
-        """ Delete a user.
+        """Delete a user.
 
         Deletion will cascade through the database and remove their records
         of attendance, registrations, and presentations. Be careful.
@@ -140,17 +103,13 @@ class UserAPI(MethodView):
         Returns:
             dict: status of the deletion.
         """
-        if current_user.usertype_id != 1:
-            abort(401)
-        
-        if current_user.id == user_id:
-            abort(403)
-
-        args = parser.parse(UserSchema(), location="query")
         user = User.query.get(user_id)
 
         if user is None:
             abort(404)
+
+        if user == current_user:
+            abort(403)
 
         try:
             db.session.delete(user)
@@ -163,7 +122,7 @@ class UserAPI(MethodView):
 
 class UserLocationAPI(MethodView):
     def get(self: None, user_id: int) -> User:
-        """ Get a user's location
+        """Get a user's location
 
         Args:
             user_id (int): valid user ID
@@ -177,8 +136,9 @@ class UserLocationAPI(MethodView):
 
         return jsonify(UserLocationSchema().dump(user.location))
 
+    @admin_or_self
     def post(self: None, user_id: int) -> User:
-        """ Update a user's location
+        """Update a user's location
 
         Args:
             user_id (int): valid user ID
@@ -186,23 +146,21 @@ class UserLocationAPI(MethodView):
         Returns:
             User: Updated user location as JSON
         """
-        args = parser.parse(NewUserLocation(), location="json")
-        
-        if current_user.id == user_id or current_user.usertype_id == 1:   
-            user = User.query.get(user_id)
-            if user is None:
-                abort(404)
+        args = parser.parse(NewUserLocation(), location="form")
 
-            try:
-                user.update(args)
-                return jsonify(UserLocationSchema().dump(user.location))
-            except Exception as e:
-                return jsonify(e)
-        else:
-            abort(403)
+        user = User.query.get(user_id)
+        if user is None:
+            abort(404)
 
+        try:
+            user.update(args)
+            return jsonify(UserLocationSchema().dump(user.location))
+        except Exception as e:
+            return jsonify(e)
+
+    @admin_or_self
     def delete(self: None, user_id: int) -> User:
-        """ Delete a user's location
+        """Delete a user's location
 
         Args:
             user_id (int): valid user ID
@@ -210,22 +168,20 @@ class UserLocationAPI(MethodView):
         Returns:
             User: User as JSON
         """
-        if current_user.id == user_id or current_user.usertype_id == 1:    
-            user = User.query.get(user_id)
-            if user is None:
-                abort(404)
+        user = User.query.get(user_id)
+        if user is None:
+            abort(404)
 
-            user.location = None
-            db.session.commit()
+        user.location = None
+        db.session.commit()
 
-            return jsonify(UserLocationSchema().dump(user.location))
-        else:
-            abort(403)
+        return jsonify(UserLocationSchema().dump(user.location))
 
 
 class UserAttendingAPI(MethodView):
+    @admin_or_self
     def get(self: None, user_id: int) -> List[Course]:
-        """ Get events where user is listed as an attendee.
+        """Get events where user is listed as an attendee.
 
         Args:
             user_id (int): valid user ID
@@ -234,47 +190,44 @@ class UserAttendingAPI(MethodView):
             List[Course]: list of courses
         """
         # Note to future Brian: using `is` works for low integers, not for large.
-        if current_user.usertype_id == 1 or current_user.usertype_id == 2 or user_id == current_user.id:
-            user = User.query.get(user_id)
-            if user is None:
-                abort(404)
-        
-            if request.headers.get('HX-Request'):
-                template = 'registrations/index-partial.html'
-                nav_items = None
-            else:
-                template = 'registrations/index.html'
-                nav_items = get_user_navigation()
-            
-            registrations = user.registrations.all()
 
-            for reg in registrations:
-                reg.course.available = reg.course.available_size()
-                if reg.attended:
-                    reg.course.state = 'attended'
-                    reg.course.icon = attended
-                elif current_user.is_enrolled(reg.course) and not reg.attended:
-                    reg.course.state = 'registered'
-                    reg.course.icon = registered
-                else:
-                    reg.course.state = 'available'
-            
-            sorted_regs = sorted([reg.course for reg in registrations], reverse=True)
+        user = User.query.get(user_id)
+        if user is None:
+            abort(404)
 
-            return render_template(
-                template,
-                events=SmallCourseSchema(many=True).dump(sorted_regs),
-                menuitems=nav_items
-            )
-
+        if request.headers.get("HX-Request"):
+            template = "registrations/index-partial.html"
+            nav_items = None
         else:
-            abort(401)
+            template = "registrations/index.html"
+            nav_items = get_user_navigation()
+
+        registrations = user.registrations.all()
+
+        for reg in registrations:
+            reg.course.available = reg.course.available_size()
+            if reg.attended:
+                reg.course.state = "attended"
+                reg.course.icon = attended
+            elif current_user.is_enrolled(reg.course) and not reg.attended:
+                reg.course.state = "registered"
+                reg.course.icon = registered
+            else:
+                reg.course.state = "available"
+
+        sorted_regs = sorted([reg.course for reg in registrations], reverse=True)
+
+        return render_template(
+            template,
+            events=SmallCourseSchema(many=True).dump(sorted_regs),
+            menuitems=nav_items,
+        )
 
 
 class UserConfirmedAPI(MethodView):
     # Return only _confirmed_ courses for a user
     def get(self: None, user_id: int) -> List[Course]:
-        """ Get a list of sessions for which the user's attendance has been confirmed.
+        """Get a list of sessions for which the user's attendance has been confirmed.
 
         Args:
             user_id (int): User ID
@@ -282,14 +235,21 @@ class UserConfirmedAPI(MethodView):
         Returns:
             List[Course]: List of <Course> objects as JSON
         """
-        if user_id == current_user.id or current_user.usertype_id == 1 or current_user.usertype_id == 2:
-            confirmed = CourseUserAttended.query.filter_by(user_id=user_id, attended=1).all()
+        if (
+            user_id == current_user.id
+            or current_user.usertype_id == 1
+            or current_user.usertype_id == 2
+        ):
+            confirmed = CourseUserAttended.query.filter_by(
+                user_id=user_id, attended=1
+            ).all()
             if confirmed is None:
                 abort(404)
 
             for event in confirmed:
-                event.course.total = ceil((event.course.ends - event.course.starts).total_seconds() / 3600)
-                # event.course.total = divmod((event.course.ends - event.course.starts).total_seconds(), 3600)[0]
+                event.course.total = ceil(
+                    (event.course.ends - event.course.starts).total_seconds() / 3600
+                )
 
             return jsonify(UserAttendingSchema(many=True).dump(confirmed))
         else:
@@ -298,8 +258,9 @@ class UserConfirmedAPI(MethodView):
 
 class UserPresentingAPI(MethodView):
     # Show courses a user is presenting
+    @admin_or_self
     def get(self: None, user_id: int) -> List[Course]:
-        """ Return a list of Courses where the user_id is listed as a presenter.
+        """Return a list of Courses where the user_id is listed as a presenter.
 
         Args:
             user_id (int): User ID
@@ -307,11 +268,8 @@ class UserPresentingAPI(MethodView):
         Returns:
             List[Course]: List of <Course> objects as JSON
         """
-        if user_id == current_user.id or current_user.usertype_id == 1:
-            user = User.query.get(user_id)
-            if user is None:
-                abort(404)
-        else:
-            abort(401)
-        
+        user = User.query.get(user_id)
+        if user is None:
+            abort(404)
+
         return jsonify(CourseSchema(many=True).dump(user.presenting))
