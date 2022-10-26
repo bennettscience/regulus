3582762c60007efefbc7bdbfc24fd8793557bde4
bennettscience <brian.bennett2@gmail.com>
2022-10-26 15:22:22 -0400

    start util function tests

--- app/utils.py
+++ app/utils.py
@@ -16,87 +16,91 @@ from app.static.assets.icons import (
     admin,
     users,
     create,
-    logout
+    logout,
 )
 
 # Possible navigation items for a given user.
 permissions = {
-    "User": ['schedule', 'documents', 'logout'],
+    "User": ["schedule", "documents", "logout"],
     "Presenter": [
-        'schedule',
-        'documents',
-        'admin',
-        'create',
-        'logout',
+        "schedule",
+        "documents",
+        "admin",
+        "create",
+        "logout",
     ],
     "SuperAdmin": [
-        'schedule',
-        'documents',
-        'admin',
-        'users',
-        'create',
-        'logout',
+        "schedule",
+        "documents",
+        "admin",
+        "users",
+        "create",
+        "logout",
     ],
 }
 
 # Store all of the navigation objects to use in a comprehension
 navigation_items = [
     {
-        "element": 'presenter',
-        "label": 'Presenter Dashboard',
-        "href": '/presenter',
+        "element": "presenter",
+        "label": "Presenter Dashboard",
+        "href": "/presenter",
         "icon": presenter,
     },
     {
-        "element": 'reports',
-        "label": 'Reports',
-        "href": '/reports',
+        "element": "reports",
+        "label": "Reports",
+        "href": "/reports",
         "icon": reports,
     },
     {
-        "element": 'admin',
-        "label": 'Event Management',
-        "href": '/admin/events',
+        "element": "admin",
+        "label": "Event Management",
+        "href": "/admin/events",
         "icon": admin,
     },
     {
-        "element": 'users',
-        "label": 'User Management',
-        "href": '/admin/users',
+        "element": "users",
+        "label": "User Management",
+        "href": "/admin/users",
         "icon": users,
     },
     {
-        "element": 'create',
-        "label": 'Create Event',
-        "href": '/create',
+        "element": "create",
+        "label": "Create Event",
+        "href": "/create",
         "icon": create,
-        "action": 'on htmx:afterSwap call makeQuill() end'
+        "action": "on htmx:afterSwap call makeQuill() end",
     },
-    {
-        "element": 'logout',
-        "label": 'Logout',
-        "href": '/logout',
-        "icon": logout
-    },
+    {"element": "logout", "label": "Logout", "href": "/logout", "icon": logout},
 ]
+
 
 def get_user_navigation_menu() -> List[dict]:
     user_type = current_user.role.name
-    items = [menuitem for menuitem in navigation_items if menuitem['element'] in permissions[user_type]]
+    items = [
+        menuitem
+        for menuitem in navigation_items
+        if menuitem["element"] in permissions[user_type]
+    ]
     return items
 
+
 def clean_escaped_html(value: str) -> str:
-    """ Remove non-whitelist HTML tags from user input.
+    """Remove non-whitelist HTML tags from user input.
 
-    On the frontend, Jodit escapes HTML characters on the fly, sending unicode strings. Those need
-    to be removed before processing with bleach to remove non-whitelisted tags.
+    On the frontend, Jodit escapes HTML characters on the fly,
+    sending unicode strings.
+    Those need to be removed before processing with bleach to
+    remove non-whitelisted tags.
 
-    This is a little goofy because the escaped HTML needs to be parsed back into unicode characters
-    to be removed correctly by bleach.
+    This is a little goofy because the escaped HTML needs to be
+    parsed back into unicode characters to be removed correctly by bleach.
 
-    The bleach_extras package removes non-whitelisted tag children elements as well as the tags.
+    The bleach_extras package removes non-whitelisted tag children
+    elements as well as the tags.
 
-    This solution is a combination of approaches from 
+    This solution is a combination of approaches from
       - https://stackoverflow.com/questions/701704/convert-html-entities-to-unicode-and-vice-versa
       - https://github.com/jvanasco/bleach_extras
 
@@ -106,15 +110,18 @@ def clean_escaped_html(value: str) -> str:
     Returns:
         str: Sanitized HTML
     """
-    clean = bleach_extras.clean_strip_content(unescape(value), tags=['p', 'strong', 'em', 'u', 'br', 'ul', 'ol', 'li'])
+    clean = bleach_extras.clean_strip_content(
+        unescape(value), tags=["br", "p", "strong", "em", "u", "ul", "ol", "li"]
+    )
     return clean
 
+
 def object_to_select(items):
-    """ Unpack a database class for the select partial
+    """Unpack a database class for the select partial
 
     Args:
         items (any): List of database objects
-    
+
     Returns:
         list (object): [{text, value}, ...] formatted list
     """
@@ -122,29 +129,38 @@ def object_to_select(items):
 
     return results
 
+
 def get_user_navigation():
-    """ Page refreshes need to rebuild the user menu. This gets the current user and adds their
-        menu options to the response before being sent back to the client.
+    """
+    Page refreshes need to rebuild the user menu.
+    This gets the current user and adds their
+    menu options to the response before being sent back to the client.
     """
     if current_user.is_anonymous:
         nav_items = []
     else:
         nav_items = get_user_navigation_menu()
-    
-    # If the user session isn't fresh, they need to log in again.
-    if not current_user.is_anonymous and session['_fresh']:
 
-        nav_items.insert(0, {
-            "element": 'schedule',
-            "label": "My Schedule",
-            "href": "/users/{}/registrations".format(current_user.id),
-            "icon": calendar
-        })
-        nav_items.insert(1, {
-            "element": 'documents',
-            "label": 'Account & Documents',
-            "href": '/users/{}/documents'.format(current_user.id),
-            "icon": documents,
-        })
+    # If the user session isn't fresh, they need to log in again.
+    if not current_user.is_anonymous and session["_fresh"]:
+
+        nav_items.insert(
+            0,
+            {
+                "element": "schedule",
+                "label": "My Schedule",
+                "href": "/users/{}/registrations".format(current_user.id),
+                "icon": calendar,
+            },
+        )
+        nav_items.insert(
+            1,
+            {
+                "element": "documents",
+                "label": "Account & Documents",
+                "href": "/users/{}/documents".format(current_user.id),
+                "icon": documents,
+            },
+        )
 
     return nav_items
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,47 @@
-# Get the user navigation menus
+import os
+from unittest import skip
+
+from app import app, db
+from sqlalchemy import Table
+
+from app.utils import get_user_navigation
+from tests.utils import captured_templates, Loader, TestBase
+
+
+class TestUtils(TestBase):
+    def setUp(self):
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+        app.config["TESTING"] = True
+        db.create_all()
+
+        fixtures = [
+            "roles.json",
+            "users.json",
+        ]
+
+        self.client = app.test_client()
+
+        conn = db.engine.connect()
+        metadata = db.metadata
+
+        for filename in fixtures:
+            filepath = os.path.join(app.config.get("FIXTURES_DIR"), filename)
+            if os.path.exists(filepath):
+                data = Loader.load(filepath)
+                table = Table(data[0]["table"], metadata)
+                conn.execute(table.insert(), data[0]["records"])
+            else:
+                raise IOError(
+                    "Error loading '{0}'. File could not be found".format(filename)
+                )
+
+    def tearDown(self):
+        db.drop_all()
+        db.session.close()
+
+    # Anonymous users don't get extra nav items
+    def test_get_user_navigation(self):
+
+        with app.test_request_context():
+            nav = get_user_navigation()
+            self.assertEqual(len(nav), 0)
