159aef5a23aa300571d638b4812080b7a0ed29b4
bennettscience <brian.bennett2@gmail.com>
2022-10-26 14:37:52 -0400

    user route tests passing

--- app/blueprints/users_blueprint.py
+++ app/blueprints/users_blueprint.py
@@ -6,40 +6,39 @@ from app.models import UserType, User
 from webargs import fields
 from webargs.flaskparser import parser
 
-from app.wrappers import admin_or_self
+from app.wrappers import admin_only
 from app.utils import get_user_navigation
 
 from resources.users import (
     UserAPI,
     UserAttendingAPI,
-    UserListAPI,
     UserLocationAPI,
     UserPresentingAPI,
-    UserConfirmedAPI
+    UserConfirmedAPI,
 )
 
 from app.schemas import UserRoleSchema, UserSchema
 
-users_bp = Blueprint('users_bp', __name__)
+users_bp = Blueprint("users_bp", __name__)
 
-users_view = UserListAPI.as_view("users_api")
 user_view = UserAPI.as_view("user_api")
 user_location_view = UserLocationAPI.as_view("user_location_api")
 user_attending_view = UserAttendingAPI.as_view("user_attending_api")
 user_confirmed_view = UserConfirmedAPI.as_view("user_confirmed_api")
 user_presenting_view = UserPresentingAPI.as_view("user_presenting_api")
 
-@users_bp.get('/admin/users')
-@admin_or_self
+
+@users_bp.get("/admin/users")
+@admin_only
 def index():
-    """ Return users based on the current_user and the optional
+    """Return users based on the current_user and the optional
 
     Returns:
         _type_: _description_
     """
-    args = parser.parse({
-        'usertype_id': fields.Int(missing=None)
-    }, location='querystring')
+    args = parser.parse(
+        {"usertype_id": fields.Int(missing=None)}, location="querystring"
+    )
 
     nav_items = get_user_navigation()
 
@@ -48,11 +47,15 @@ def index():
 
     # If a usertype id is in the request query, return users for that role.
     # Otherwise, return only the select field to filter down.
-    if args['usertype_id'] is not None:
-        template = 'users/partials/user-table-rows.html'
+    if args["usertype_id"] is not None:
+        template = "users/partials/user-table-rows.html"
 
-        if args['usertype_id'] != 0:
-            query = sorted(User.query.filter(User.usertype_id == args['usertype_id']).order_by(User.name.asc()).all())
+        if args["usertype_id"] != 0:
+            query = sorted(
+                User.query.filter(User.usertype_id == args["usertype_id"])
+                .order_by(User.name.asc())
+                .all()
+            )
         else:
             query = sorted(User.query.order_by(User.name.asc()).all())
 
@@ -61,25 +64,22 @@ def index():
             "options": options,
         }
     else:
-        if request.headers.get('HX-Request'):
-            template = 'users/index-partial.html'
+        if request.headers.get("HX-Request"):
+            template = "users/index-partial.html"
         else:
-            template = 'users/index.html'
+            template = "users/index.html"
 
         query = sorted(User.query.order_by(User.name.asc()).all())
-        
+
         content = {
             "options": options,
             "name": "usertype_id",
             "users": UserSchema(many=True).dump(query),
-            "menuitems": nav_items
+            "menuitems": nav_items,
         }
 
     return render_template(template, **content)
 
-
-# Create new usertypes
-# users_bp.add_url_rule("/users/<int:usertype_id>", view_func=users_view, methods=["GET", "POST"])
 
 users_bp.add_url_rule(
     "/users/<int:user_id>", view_func=user_view, methods=["GET", "PUT", "DELETE"]
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,133 +1,112 @@
 import json
-import unittest
+import os
 
-from flask_login.utils import login_user
-
+from sqlalchemy import Table
 from app import app, db
 from app.models import Course, CourseUserAttended, Location, User, UserType
+from tests.utils import TestBase, Loader, captured_templates
 
 
-class TestUsers(unittest.TestCase):
-    @app.route('/auto_login/<username>')
-    def auto_login(username):
-        user = User.query.filter_by(name=username).first()
-        login_user(user, remember=True)
-        return "ok"
-
-    def login(self, username):
-        response = self.client.get(f"/auto_login/{username}")
-
+class TestUsers(TestBase):
     def setUp(self) -> None:
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+        app.config["TESTING"] = True
         db.create_all()
-        
-        # Set up user roles to test with
-        super_admin_user = User(name="SuperAdmin", location_id=1, usertype_id=1)
-        presenter_user = User(name="Presenter", location_id=1, usertype_id=2)
-        user = User(name="User", location_id=1, usertype_id=4)
-        db.session.add_all([super_admin_user, presenter_user, user])
-        db.session.commit()
-        
+
+        fixtures = [
+            "courses.json",
+            "locations.json",
+            "roles.json",
+            "users.json",
+        ]
+
         self.client = app.test_client()
 
-        c1 = Course(title="Course 1", course_size=10)
-        u2 = User(name="User 2", location_id=2, usertype_id=2)
-        l1 = Location(name="Building 1", address="123 Main")
-        l2 = Location(name="Building 2", address="999 Oak")
-        ut1 = UserType(name="Type1", description="The first user type")
-        ut2 = UserType(name="Type2", description="The second user type")
+        conn = db.engine.connect()
+        metadata = db.metadata
 
-        db.session.add_all([c1, u2, l1, l2, ut1, ut2])
-        c1.presenters.append(presenter_user)
-        db.session.commit()
+        for filename in fixtures:
+            filepath = os.path.join(app.config.get("FIXTURES_DIR"), filename)
+            if os.path.exists(filepath):
+                data = Loader.load(filepath)
+                table = Table(data[0]["table"], metadata)
+                conn.execute(table.insert(), data[0]["records"])
+            else:
+                raise IOError(
+                    "Error loading '{0}'. File could not be found".format(filename)
+                )
 
     def tearDown(self):
-        db.session.remove()
         db.drop_all()
+        db.session.close()
 
+    # Request all users as an admin
+    # A missing querystring defaults to all users in the database
     def test_get_users_as_admin(self):
-        self.login("SuperAdmin")
-        resp = self.client.get("/users")
-        self.assertTrue(resp.status_code == 200)
-        self.assertIsInstance(resp.json, list)
-        self.assertEqual(len(resp.json), 4)
-    
-    # When requesting as a non-admin, user_type is a required param, otherwise 401
-    def test_get_users_as_presenter(self):
+        self.login("Admin")
+
+        with captured_templates(app) as templates:
+            resp = self.client.get("/admin/users")
+
+            self.assertEqual(resp.status_code, 200)
+
+            names = [template["template_name"] for template in templates]
+            self.assertIn("users/index.html", names)
+            for template in templates:
+                if template["template_name"] == "users/partials/user-table-rows":
+                    context = template["context"]
+                    self.assertEqual(len(context["users"]), 3)
+
+    # request only users of a certain type
+    def test_get_users_of_type(self):
+        self.login("Admin")
+
+        with captured_templates(app) as templates:
+            resp = self.client.get("/admin/users?usertype_id=2")
+
+            self.assertEqual(resp.status_code, 200)
+
+            for template in templates:
+                if template["template_name"] == "users/partials/user-table-row.html":
+                    context = template["context"]
+                    # There is only one user with that type
+                    self.assertEqual(len(context["users"]), 1)
+
+    def test_get_users_as_non_admin(self):
         self.login("Presenter")
-        resp = self.client.get("/users")
-        self.assertTrue(resp.status_code == 401)
-        # self.assertIsInstance(resp.json, list)
-        # self.assertEqual(len(resp.json), 2)
-    
+
+        resp = self.client.get("/admin/users")
+        self.assertEqual(resp.status_code, 403)
+
     # When requesting as a non-admin, user_type is a required param, otherwise 401
-    def test_get_users_as_presenter_with_querystring(self):
+    # TODO: This just gets the sidebar, test the route to actually add a user
+    def test_add_presenter_as_presenter(self):
         self.login("Presenter")
-        resp = self.client.get("/users?user_type=2")
-        self.assertTrue(resp.status_code == 200)
-        self.assertIsInstance(resp.json, list)
-        self.assertEqual(len(resp.json), 2)
-    
-    # When requesting as a non-admin, user_type is a required param, otherwise 401
-    def test_get_users_as_user(self):
+        resp = self.client.get("/admin/events/1/presenters/edit")
+        self.assertEqual(resp.status_code, 200)
+
+    # When requesting as a non-admin, return 403
+    def test_get_users_as_user_no_type(self):
         self.login("User")
-        resp = self.client.get("/users?user_type=2")
-        self.assertTrue(resp.status_code == 401)
-
-    def test_post_user(self):
-        self.login("SuperAdmin")
-        headers = {"Content-Type": "application/json"}
-        payload = {
-            "name": "User 3",
-            "location_id": 1,
-            "usertype_id": 1,
-            "email": "something@email.com",
-        }
-        resp = self.client.post("/users", data=json.dumps(payload), headers=headers)
-        self.assertTrue(resp.status_code == 200)
-        self.assertTrue(resp.json["name"] == "User 3")
-    
-    def test_post_user_non_admin(self):
-        self.login("Presenter")
-        headers = {"Content-Type": "application/json"}
-        payload = {
-            "name": "User 3",
-            "location_id": 1,
-            "usertype_id": 1,
-            "email": "something@email.com",
-        }
-        resp = self.client.post("/users", data=json.dumps(payload), headers=headers)
-        self.assertTrue(resp.status_code == 403)
-
-    def test_post_bad_user(self):
-        self.login("SuperAdmin")
-        headers = {"Content-Type": "application/json"}
-        payload = {"name": "Bad user", "location_id": 1, "usertype_id": 1}
-        resp = self.client.post("/users", data=json.dumps(payload), headers=headers)
-        self.assertTrue(resp.status_code == 422)
-        self.assertEqual(
-            resp.json["messages"]["email"][0], "Missing data for required field."
-        )
+        resp = self.client.get("/admin/users")
+        self.assertEqual(resp.status_code, 403)
 
     def test_get_user_as_self(self):
         self.login("User")
         resp = self.client.get("/users/3")
-        self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "User")
+        self.assertEqual(resp.status_code, 200)
 
     def test_get_user_as_admin(self):
-        self.login("SuperAdmin")
+        self.login("Admin")
         # request a non-self user
         resp = self.client.get("/users/3")
-        self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "User")
-    
+        self.assertEqual(resp.status_code, 200)
+
     def test_get_user_as_presenter(self):
         self.login("Presenter")
         resp = self.client.get("/users/3")
-        self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "User")
-    
+        self.assertEqual(resp.status_code, 403)
+
     # Users cannot see other users' information
     def test_get_user_forbidden(self):
         self.login("User")
@@ -135,103 +114,92 @@ class TestUsers(unittest.TestCase):
         self.assertTrue(resp.status_code == 403)
 
     def test_put_user(self):
-        self.login("SuperAdmin")
-        headers = {"Content-Type": "application/json"}
+        self.login("Admin")
         payload = {"name": "My new name"}
-        resp = self.client.put("/users/1", data=json.dumps(payload), headers=headers)
-        self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "My new name")
-    
+
+        resp = self.client.put("/users/1", data=payload)
+        self.assertEqual(resp.status_code, 200)
+
     # Non-superadmin users can edit themselves
     def test_put_user_self(self):
         self.login("User")
-        headers = {"Content-Type": "application/json"}
         payload = {"name": "My new name"}
-        resp = self.client.put("/users/3", data=json.dumps(payload), headers=headers)
+        resp = self.client.put("/users/3", data=payload)
         self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "My new name")
 
     def test_bad_put_user(self):
-        self.login("SuperAdmin")
-        headers = {"Content-Type": "application/json"}
+        self.login("Admin")
         payload = {"flame": "My new name"}
-        resp = self.client.put("/users/1", data=json.dumps(payload), headers=headers)
+        resp = self.client.put("/users/1", data=payload)
         self.assertTrue(resp.status_code == 422)
-        self.assertEqual(resp.json["messages"]["flame"], ["Unknown field."])
-    
+
     def test_put_user_forbidden(self):
         self.login("Presenter")
-        headers = {"Content-Type": "application/json"}
         payload = {"name": "My new name"}
-        resp = self.client.put("/users/1", data=json.dumps(payload), headers=headers)
+        resp = self.client.put("/users/1", data=payload)
         self.assertTrue(resp.status_code == 403)
 
     # Only superadmins can delete users
     def test_delete_user(self):
-        self.login("SuperAdmin")
-        user = User.query.filter(User.name == 'User 2').first()
+        self.login("Admin")
+        user = User.query.filter(User.name == "User").first()
         resp = self.client.delete(f"/users/{user.id}")
         self.assertTrue(resp.status_code == 200)
         self.assertEqual(resp.json["message"], "Delete successful.")
-    
+
     # Non-admins cannot delete users
     def test_delete_user_forbidden_presenter(self):
         self.login("User")
-        user = User.query.filter(User.name == 'User 2').first()
+        user = User.query.filter(User.name == "User").first()
         resp = self.client.delete(f"/users/{user.id}")
-        self.assertTrue(resp.status_code == 401)
-    
+        self.assertTrue(resp.status_code == 403)
+
     # Non-admins cannot delete users
     def test_delete_user_forbidden_user(self):
         self.login("Presenter")
-        user = User.query.filter(User.name == 'User 2').first()
+        user = User.query.filter(User.name == "User").first()
         resp = self.client.delete(f"/users/{user.id}")
-        self.assertTrue(resp.status_code == 401)
+        self.assertTrue(resp.status_code == 403)
 
     # superadmins cannot delete themselves
     def test_delete_user_admin(self):
-        self.login("SuperAdmin")
-        user = User.query.filter(User.name == "SuperAdmin").first()
+        self.login("Admin")
+        user = User.query.filter(User.name == "Admin").first()
         resp = self.client.delete(f"/users/{user.id}")
         self.assertTrue(resp.status_code == 403)
 
     # No access control on this method to test
     def test_get_user_location(self):
-        self.login("SuperAdmin")
+        self.login("Admin")
         resp = self.client.get("/users/1/locations")
         self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "Building 1")
+        self.assertEqual(resp.json["name"], "Location 1")
 
-    # Superadmins can change locations
+    # Superadmins can change user locations
     def test_post_user_location_admin(self):
-        self.login("SuperAdmin")        
-        headers = {"Content-Type": "application/json"}
+        self.login("Admin")
         payload = {"location_id": 2}
         # Check a non-self user id
-        resp = self.client.post(
-            "users/2/locations", data=json.dumps(payload), headers=headers
-        )
+        resp = self.client.post("users/3/locations", data=payload)
         self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "Building 2")
+        self.assertEqual(resp.json["name"], "Location 2")
 
     # Users can change their own location
     def test_post_user_location_self(self):
         self.login("User")
-        headers = {"Content-Type": "application/json"}
         payload = {"location_id": 2}
-        resp = self.client.post(
-            "users/3/locations", data=json.dumps(payload), headers=headers
-        )
+        resp = self.client.post("users/3/locations", data=payload)
         self.assertTrue(resp.status_code == 200)
-        self.assertEqual(resp.json["name"], "Building 2")
+        self.assertEqual(resp.json["name"], "Location 2")
 
-    # Superadmin test delete any location
+    # Superadmin can delete any location
     def test_delete_user_location(self):
-        self.login("SuperAdmin")
+        self.login("Admin")
         resp = self.client.delete("/users/2/locations")
         self.assertTrue(resp.status_code == 200)
         self.assertEqual(resp.json, {})
-    
+
+    # Users can remove their own location.
     def test_delete_user_location_self(self):
         self.login("User")
         resp = self.client.delete("/users/3/locations")
@@ -246,12 +214,19 @@ class TestUsers(unittest.TestCase):
         db.session.commit()
 
         self.login("User")
-        resp = self.client.get("/users/3/registrations")
-        self.assertEqual(resp.status_code, 200)
-        self.assertIsInstance(resp.json, list)
-        self.assertEqual(len(resp.json), 1)
-        self.assertEqual(resp.json[0]["attended"], False)
-        self.assertEqual(resp.json[0]["course"]["title"], "Course 1")
+
+        with captured_templates(app) as templates:
+            resp = self.client.get("/users/3/registrations")
+            self.assertEqual(resp.status_code, 200)
+
+            names = [template["template_name"] for template in templates]
+            self.assertIn("registrations/index.html", names)
+
+            for template in templates:
+                if template["template_name"] == "registrations/index.html":
+                    context = template["context"]
+
+                    self.assertEqual(len(context["events"]), 1)
 
     # Admins can get lists of attendees for any session
     def test_user_attending_admin(self):
@@ -260,16 +235,27 @@ class TestUsers(unittest.TestCase):
         course.registrations.append(CourseUserAttended(course_id=1, user_id=3))
         db.session.commit()
 
-        self.login("SuperAdmin")
-        resp = self.client.get("/users/3/registrations")
-        self.assertEqual(resp.status_code, 200)
-        self.assertIsInstance(resp.json, list)
-        self.assertEqual(len(resp.json), 1)
-        self.assertEqual(resp.json[0]["attended"], False)
-        self.assertEqual(resp.json[0]["course"]["title"], "Course 1")
-    
+        self.login("Admin")
+
+        with captured_templates(app) as templates:
+            resp = self.client.get("/users/3/registrations")
+            self.assertEqual(resp.status_code, 200)
+
+            names = [template["template_name"] for template in templates]
+            self.assertIn("registrations/index.html", names)
+
+            for template in templates:
+                if template["template_name"] == "registrations/index.html":
+                    context = template["context"]
+
+                    self.assertEqual(len(context["events"]), 1)
+
     # Users can get their own list of presenting sessions
     def test_user_presenting(self):
+        course = Course.query.get(1)
+        course.presenters.append(User.query.get(2))
+        db.session.commit()
+
         self.login("Presenter")
         resp = self.client.get("/users/2/presenting")
         self.assertTrue(resp.status_code == 200)
