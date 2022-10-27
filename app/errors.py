6d9e46fcb5077ab48f4cfddc8a005ccffcd333a0
bennettscience <brian.bennett2@gmail.com>
2022-10-27 09:19:47 -0400

    use session for user menu

--- app/errors.py
+++ app/errors.py
@@ -3,20 +3,21 @@ from flask import jsonify, render_template
 from app import app
 from app.utils import get_user_navigation
 
+
 @app.errorhandler(401)
 def unauthorized(err):
-    nav_items = get_user_navigation()
-    return render_template('shared/errors/401.html'), 401
+    return render_template("shared/errors/401.html"), 401
+
 
 @app.errorhandler(403)
 def forbidden(err):
-    nav_items = get_user_navigation()
-    return render_template('shared/errors/403.html', menuitems=nav_items), 403
+    return render_template("shared/errors/403.html"), 403
+
 
 @app.errorhandler(404)
 def page_not_found(err):
-    nav_items = get_user_navigation()
-    return render_template('shared/errors/404.html', menuitems=nav_items), 404
+    return render_template("shared/errors/404.html"), 404
+
 
 @app.errorhandler(409)
 def request_conflict(err):
@@ -25,11 +26,12 @@ def request_conflict(err):
         {
             "code": err.code,
             "name": err.name,
-            "description": "There aren't enough seats left to register for the event."
+            "description": "There aren't enough seats left to register for the event.",
         }
     )
     response.content_type = "application/json"
     return response
+
 
 @app.errorhandler(422)
 @app.errorhandler(400)
@@ -42,6 +44,7 @@ def handle_error(err):
     else:
         return jsonify({"errors": messages}), err.code
 
+
 @app.errorhandler(500)
 def internal_error(e):
     response = e.get_response()
@@ -49,7 +52,7 @@ def internal_error(e):
         {
             "code": e.code,
             "name": e.name,
-            "description": "Something went wrong on our end. We'll get on that now. Sorry."
+            "description": "Something went wrong on our end. We'll get on that now. Sorry.",
         }
     )
     response.content_type = "applicaton/json"
